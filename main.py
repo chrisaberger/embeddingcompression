3771923a5f9fab7a49e3da5ecf795d821fd832e9
Christopher Aberger <christopheraberger@Christophers-MacBook-Pro-2.local>
2018-07-06 22:41:07 -0700

    Updated columnwise method.

--- main.py
+++ main.py
@@ -21,87 +21,97 @@ def load_embeddings(filename):
                       na_values=[''] )
     vocab = df[df.columns[0]]
     embedding = df.drop(df.columns[0], axis=1).as_matrix()
+    print(embedding)
     print("Embedding shape: " + str(embedding.shape))
     return vocab, embedding
 
-def rowwise_kmeans(X, n_clusters):
-    kmeans = KMeans(n_clusters=n_clusters).fit(X)
+def bucket_rows(X, n_buckets):
+    t0 = time.time()
 
-    for i in tqdm(range(X.shape[0])):
-        row = X[i, :]
+    # Run kmeans to determine the buckets.
+    kmeans = KMeans(n_clusters=n_buckets).fit(X)
+    print("KMeans Bucket Rows Time: " + str(time.time()-t0))
 
+    # Create the buckets and sort the vocabulary.    
+    buckets = {}
+    index_buckets = {} # original row index mapping
+    new_V = []
+    for i in range(len(kmeans.labels_)):
+        label = kmeans.labels_[i]
+        if label not in buckets:
+            buckets[label] = [X[i,:]]
+            index_buckets[label] = [i]
+        else:
+            buckets[label].append(X[i,:])
+            index_buckets[label].append(i)
+    
+    """
+    print(kmeans.cluster_centers_[0])
+    for i in range(n_buckets):
+        print(i)
+        print(np.array(index_buckets[i]))
+    """
+    return buckets, index_buckets
+
+def columnwise_kmean_compression(X, num_centroids_per_column):
+    X_t = np.transpose(X)
+    compressed_X_t = np.zeros(X_t.shape)
+    for i in tqdm(range(X_t.shape[0])):
+        row = X_t[i, :]
+        kmeans = KMeans(n_clusters=num_centroids_per_column)\
+                                            .fit(row.reshape(-1, 1))
+        compressed_X_t[i, :] = kmeans.cluster_centers_[kmeans.labels_] \
+                                        .reshape(1, compressed_X_t.shape[1])
+    return np.transpose(compressed_X_t)
+
+def print_stats(X, baseline_frob_norm = None):
+    frob_norm = np.linalg.norm(X)
+    one_d_X = X.flatten()
+    print()
+    print("Frobenius Norm of Input: " + str(frob_norm))
+    print("Mean of Input: " + str(np.mean(one_d_X)))
+    print("Standard Deviation of Input: " + str(np.std(one_d_X)))
+    if baseline_frob_norm is not None:
+        print("Frob norm diff: " + str(np.abs( frob_norm - baseline_frob_norm )))
+    print()
+    return frob_norm
+
+def to_file(filename, V, X):
+    print(X)
+    pdv = pd.DataFrame(V)
+    pdv.rename(columns={0:'v'}, inplace=True)
+    result = pdv.join(pd.DataFrame(X))
+    result.to_csv(filename, sep=' ', index=False, header=False)
 
 filename = "../GloVe-1.2/vectors.txt"
-load_embeddings(filename)
-sorted_embedding = embedding#np.sort(embedding)
-sort_indexes = np.argsort(embedding)
+vocab, embedding = load_embeddings(filename)
+frob_norm = print_stats(embedding)
 
-print(sorted_embedding)
+# Bucket method
+buckets, index_buckets = bucket_rows(embedding, 200)
+compressed_embedding = None
+new_vocab = None
+for index in buckets:
+    bucket = np.array(buckets[index])
+    new_vals = columnwise_kmean_compression(bucket, 2)
+    if compressed_embedding is None:
+        compressed_embedding = new_vals
+        new_vocab = vocab[np.array(index_buckets[index])].as_matrix()
+    else:
+        compressed_embedding = np.concatenate((compressed_embedding, new_vals))
+        new_vocab = np.concatenate((new_vocab, vocab[np.array(index_buckets[index])].as_matrix()))
 
-median = np.median(sorted_embedding, axis=0)
-mean = np.mean(sorted_embedding, axis=0)
-max_vals = np.max(sorted_embedding, axis=0)
-min_vals = np.min(sorted_embedding, axis=0)
+compressed_embedding = np.array(compressed_embedding)
+new_vocab = np.array(new_vocab)
 
-middle = ((max_vals-min_vals)/2) + min_vals
-sorted_embedding -= middle
+print(compressed_embedding.shape)
+compressed_embedding = np.array(compressed_embedding)
+print_stats(compressed_embedding, frob_norm)
+to_file("bucketwise.txt", new_vocab, compressed_embedding)
+# end bucket method
 
-frob_norm = np.linalg.norm(embedding)
-one_d_embedding = embedding.flatten()
-
-input_bytes = embedding.size*4
-print("Frobenius Norm of Input: " + str(frob_norm))
-print("Mean of Input: " + str(np.mean(one_d_embedding)))
-print("Standard Deviation of Input: " + str(np.std(one_d_embedding)))
-print("Input number of bytes: " + str(embedding.size*4))
-
-print()
-
-transposed = np.transpose(sorted_embedding)
-
-codebook_size = n_clusters*transposed.shape[0]
-print()
-print("Num Clusters: " + str(codebook_size))
-
-t0 = time.time()
-
-compressed_embedding = np.zeros(transposed.shape)
-for i in tqdm(range(transposed.shape[0])):
-    row = transposed[i, :]
-    kmeans = KMeans(n_clusters=n_clusters).fit(row.reshape(-1, 1))
-    c_row = kmeans.cluster_centers_[kmeans.labels_] \
-                                    .reshape(1, compressed_embedding.shape[1])
-    compressed_embedding[i, :] = c_row
-
-t1 = time.time()
-print("KMeans time: " + str(t1-t0))
-
-# to reconstruct 
-compressed_embedding = np.transpose(compressed_embedding)
-compressed_embedding += middle
-x_inds_1d = np.arange(0, compressed_embedding.shape[0])
-x_inds = np.zeros(compressed_embedding.shape, dtype=np.int32)
-for i in range(0, compressed_embedding.shape[0]):
-    x_inds[i, :] = x_inds_1d[i]
-#compressed_embedding[x_inds, sort_indexes] = compressed_embedding
-
-print()
-print(compressed_embedding)
-print()
-
-embedding_indexes = (embedding.size*int(np.ceil(np.log2(n_clusters))))/8
-codebook_bytes = codebook_size*4
-compressed_bytes = codebook_bytes+embedding_indexes
-
-print("Frobenius Norm of Compressed: " + str(np.linalg.norm(compressed_embedding)))
-print("Mean of Compressed: " + str(np.mean(compressed_embedding)))
-print("Standard Deviation of Compressed: " + str(np.std(compressed_embedding.flatten())))
-print("Compressed Bytes: " + str(codebook_bytes+embedding_indexes))
-print()
-print("Frob norm diff: " + str(np.abs(np.linalg.norm(compressed_embedding)-frob_norm)))
-print("Ratio: " + str(input_bytes/compressed_bytes))
-
-pdv = vocab.to_frame()
-pdv.rename(columns={0:'v'}, inplace=True)
-result = pdv.join(pd.DataFrame(compressed_embedding))
-result.to_csv("output.csv", sep=' ', index=False, header=False)
+# Columnwise method
+compressed_embedding = columnwise_kmean_compression(embedding, 2)
+print_stats(compressed_embedding, frob_norm)
+to_file("columnwise.txt", vocab.as_matrix(), compressed_embedding)
+# end columnwise method
