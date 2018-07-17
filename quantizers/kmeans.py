9c3e7b6138e54a80a9d9aafe58f58e164e4c0d32
tginart <tginart@dawn17.stanford.edu>
2018-07-16 22:05:49 -0700

    incomplete work towards vect km

--- quantizers/kmeans.py
+++ quantizers/kmeans.py
@@ -3,6 +3,45 @@ from .quantizer import Quantizer
 from sklearn.cluster import KMeans
 
 
+
+class LloydMaxQuantizer(Quantizer):
+
+    def __init__(self, num_centroids, q_dim_row, q_dim_col):
+        self.num_centroids = num_centroids
+        self.q_dim_row = q_dim_row # num rows to quant together
+        self.q_dim_col = q_dim_col # num cols to quant together
+
+    def name(self):
+        return "lloydmax" + str(self.num_centroids) + "b"
+
+    def _check_dims(bucket_shape_row, bucket_shape_col):
+        if bucket_shape_row % self.q_dim_row != 0:
+            raise ValueError("bucket and quantization row dimension mismatch")
+        if bucket_shape_col % self.q_dim_cols != 0:
+            raise ValueError("bucket and quantization col dimension mismatch")
+        return 0
+            
+    def quantize(self, X):
+        #make sure the dims check out
+        num_rows_in_buck = X.shape[0]
+        num_cols_in_buck = X.shape[1]
+        _check_dims(num_rows_in_buck)
+
+        #double loop over quantized submats in the bucket
+            #across "horiztionally" first, then down "vertically"
+    
+        points = []
+        for i in range(0,
+        
+
+
+
+
+
+
+
+
+
 class KmeansQuantizer(Quantizer):
 
     def __init__(self, num_centroids):
